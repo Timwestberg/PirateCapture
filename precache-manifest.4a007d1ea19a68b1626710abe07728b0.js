4970bc40cb3c77111e063eb28906f08d33e10ea6
Tim Westberg <timwestberg43@gmail.com>
2019-01-16 22:45:48 -0800

    Updates

---
+++ precache-manifest.4a007d1ea19a68b1626710abe07728b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b3703bfee884da7871a",
+    "url": "/PirateCapture/static/js/runtime~main.5b3703bf.js"
+  },
+  {
+    "revision": "9d351edaba4c15fcaebc",
+    "url": "/PirateCapture/static/js/main.9d351eda.chunk.js"
+  },
+  {
+    "revision": "2c69ffc8bf779024886f",
+    "url": "/PirateCapture/static/js/1.2c69ffc8.chunk.js"
+  },
+  {
+    "revision": "9d351edaba4c15fcaebc",
+    "url": "/PirateCapture/static/css/main.fa2c5aaa.chunk.css"
+  },
+  {
+    "revision": "c6076ce3f612711c449418ae1eba88d7",
+    "url": "/PirateCapture/index.html"
+  }
+];
